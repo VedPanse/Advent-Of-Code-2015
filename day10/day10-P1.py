b611db6f789df7071eb7af3f0815813597a446f2
Ved Panse <75316092+VedPanse@users.noreply.github.com>
2023-06-14 21:00:46 +0530

    Add files via upload

---
+++ day10/day10-P1.py
@@ -0,0 +1,23 @@
+PUZZLE_INPUT = 1321131112
+next_input = 0
+
+def lookAndSay(num):
+    text = str(num)
+    parent_string = ''
+
+    from itertools import groupby
+    raw_list = ["".join(g) for k, g in groupby(text) if k != '-' and k != '/']
+    
+    for item in raw_list:
+        parent_string += str(len(item)) + str(item[0])
+
+    return int(parent_string)
+
+
+for i in range(40):
+    if i == 0:
+        next_input = lookAndSay(PUZZLE_INPUT)
+    else:
+        next_input = lookAndSay(next_input)
+
+print(len(str(next_input)))
